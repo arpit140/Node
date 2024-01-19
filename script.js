59c8d4cd6c0caf9ff399d96506862c4d3b5cc6be
arpit140 <arpitsunny6@gmail.com>
2024-01-19 16:42:26 +0530

    printing my name in browser using server

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+const http = require('http')
+
+
+const server = http.createServer((req,res) => {
+
+    res.end("My name is Arpit kumar singh")
+
+})
+
+
+server.listen(100)
